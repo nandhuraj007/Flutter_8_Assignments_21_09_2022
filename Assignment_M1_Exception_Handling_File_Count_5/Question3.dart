a6834283bde2022c8445cd61591b0f25235951c1
Nandhu Raj <112924887+nandhuraj007@users.noreply.github.com>
2022-09-25 13:02:34 +0530

    Update Question3.dart

--- Assignment_M1_Exception_Handling_File_Count_5/Question3.dart
+++ Assignment_M1_Exception_Handling_File_Count_5/Question3.dart
@@ -1,5 +1,41 @@
-class Myclass{
-  void samp(){
+class Exception
+
+{
+
+  Exception(String str);
+
+  Exception1()
+
+  {
+
+     Exception ne1 = Exception("Hello, I am your exception");
+
+    throw ne1;
 
   }
+
 }
+
+main()
+
+{
+
+  Exception em1 = Exception("");
+
+  try
+
+  {
+
+    em1.Exception1();
+
+  } catch(e)
+
+  {
+
+    print(e);
+
+  }
+
+}
+
+
